546e1bf00b2faa0024a3eea9e38bda17dcc21a8f
showjf <showjf2@gmail.com>
2022-10-02 15:54:03 +0900

    + - btn 생성 기능추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h3 data-testid="counter">{counter}</h3>
+        <button
+          data-testid="btnminus"
+          onClick={() => setCounter((prev) => prev - 1)}
         >
-          Learn React
-        </a>
+          {" "}
+          -{" "}
+        </button>
+        <button
+          data-testid="btnplus"
+          onClick={() => setCounter((prev) => prev + 1)}
+        >
+          {" "}
+          +{" "}
+        </button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const conuterElement = screen.getByTestId("counter");
+  expect(conuterElement).toHaveTextContent(0);
+});
+
+test("minus button has correct text", () => {
+  render(<App />);
+  const btnElement = screen.getByTestId("btnminus");
+  expect(btnElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  const btnElement = screen.getByTestId("btnplus");
+  expect(btnElement).toHaveTextContent("+");
+});
+
+test("When the + btn is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const btnElement = screen.getByTestId("btnplus");
+  fireEvent.click(btnElement);
+  const cntElement = screen.getByTestId("counter");
+  expect(cntElement).toHaveTextContent(1);
 });
